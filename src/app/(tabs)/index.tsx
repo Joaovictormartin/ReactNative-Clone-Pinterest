05891f0f1cc930667e538792c76e635b8d2f1754
Joao Victor <devjoaovictor10@gmail.com>
2024-03-13 10:14:04 -0300

    estruturacao do app

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
 import { Text, View, StyleSheet } from "react-native";
 
+import { theme } from "@/theme";
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
@@ -13,9 +15,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: theme.colors.black,
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
+    color: theme.colors.white,
+    fontFamily: theme.fontFamily.regular,
   },
 });
